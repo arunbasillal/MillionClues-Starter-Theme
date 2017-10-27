5bf4c099888fee51e7f5422ac87b5d4db3e762cc
Arun Basil Lal <arunbasillal@gmail.com>
2017-10-27 21:23:41 +0530

    Template part starter file

---
+++ template-parts/home-slider.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Homepage slider using Bootstrap 4
+ *
+ * @Since 1.0
+ * @refer https://getbootstrap.com/docs/4.0/components/carousel/
+ */
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+?>
