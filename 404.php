cdc6035aa16ce3afcc6b9307b951853f25fd86cb
Arun Basil Lal <arunbasillal@gmail.com>
2017-12-01 16:25:12 +0530

    Added default template

--- 404.php
+++ 404.php
@@ -1,5 +1,26 @@
-<?php get_header(); the_post(); ?>
+<?php
+/**
+ * 404 Page
+ *
+ * @Since 	1.0
+ */
 
-<?php the_content(); ?>
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="row d-flex justify-content-center">
+	
+		<div class="col-md-8 text-center">
+
+			<p>Sorry, this page does not exist</p>
+			
+		</div>
+		
+	</div>
+</div>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,14 +1,35 @@
+<?php
+/**
+ * WordPress Page Template
+ *
+ * @Since 	1.0
+ */
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+?>
+
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="container my-4">
+	<div class="row d-flex justify-content-center">
+	
+		<div class="col-md-8">
+		
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<h1><?php the_title() ;?></h1>	
-	<?php the_content(); ?>
+				<h1 class="text-center my-4"><?php the_title() ;?></h1>	
+				<?php the_content(); ?>
 
-<?php endwhile; else: ?>
+			<?php endwhile; else: ?>
 
-	<p>Sorry, this page does not exist</p>
+				<p>Sorry, this page does not exist</p>
 
-<?php endif; ?>
+			<?php endif; ?>
+		
+		</div>
+		
+	</div>
+</div>
 
 <?php get_footer(); ?>
